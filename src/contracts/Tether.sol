86f6c860e8418cbe43ac6d8c997a84623e74d931
chriszer tamayo <chriszer.myth@gmail.com>
2023-02-08 20:33:30 +0800

    first commit

---
+++ src/contracts/Tether.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract Tether {
+    string public name = "Tether";
+    string public symbol = "mUSDT";
+    uint256 totalSupply = 100000000000000000; // 1million tokens
+    uint8 decimals = 18;
+}
